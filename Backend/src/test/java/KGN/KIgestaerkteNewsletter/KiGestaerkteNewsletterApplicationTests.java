244e71a964c1e4170ad987d4b08c0c1f31c7da03
Alina <alina.spangenberg@gmx.ch>
2023-10-05 16:51:19 +0200

    feat(Backend): Spring Backend installiert (Spring Web & Spring Security)

---
+++ Backend/src/test/java/KGN/KIgestaerkteNewsletter/KiGestaerkteNewsletterApplicationTests.java
@@ -0,0 +1,13 @@
+package KGN.KIgestaerkteNewsletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiGestaerkteNewsletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
