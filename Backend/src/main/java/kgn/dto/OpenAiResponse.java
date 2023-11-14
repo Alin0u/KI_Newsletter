3e31ad229b158d1a6b75f7da5bb64bf9cb8b493f
Alina <alina.spangenberg@gmx.ch>
2023-11-14 23:46:11 +0100

    refactor(javadoc): Added JavaDoc

--- Backend/src/main/java/kgn/dto/OpenAiResponse.java
+++ Backend/src/main/java/kgn/dto/OpenAiResponse.java
@@ -3,9 +3,12 @@ package kgn.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-
 import java.util.List;
 
+/**
+ * The OpenAiResponse class models the response structure expected from the OpenAI API.
+ * It is designed to capture the list of completion choices provided in the response.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 public class OpenAiResponse {
