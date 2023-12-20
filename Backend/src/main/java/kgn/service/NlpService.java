d12e008e8861bb80f53ed1d926ed3adf96ea59fd
bissisi1 <bissisi1@student.zhaw.ch>
2023-12-21 00:47:13 +0100

    improved the prompts

--- Backend/src/main/java/kgn/service/NlpService.java
+++ Backend/src/main/java/kgn/service/NlpService.java
@@ -51,9 +51,11 @@ public class NlpService {
         Map<String, Object> requestBody = Map.of(
                 "model", GPT_3_5_TURBO,
                 "messages", List.of(
-                        Map.of("role", "system", "content",
-                                "As an expert in newsletter creation, your task is to develop a professional newsletter that is engaging and effectively integrates specified keywords and phrases. Please format the email in an attractive manner " +
-                                        "The tone of the newsletter should be " + style + " and the lenght should be " + lenght + "."),
+                        Map.of("role", "system", "content", "You are a newsletter-specialist who writes Newsletter-Mails out of keywords and/or phrases."),
+                        Map.of("role", "system", "content", "The newsletter must be formatted in HTML"),
+                        Map.of("role", "system", "content", "The tone of the newsletter should be " + style + " and the lenght should be " + lenght + "."),
+                        Map.of("role", "system", "content", "Your objective is to generate newsletters that avoid any words or phrases prone to triggering spam filters."),
+                        Map.of("role", "system", "content", "Additionally, each newsletter must conclude with a clear option for subscribers to unsubscribe easily. Please include a note at the end of each email"),
                         Map.of("role", "user", "content", prompt)
                 ),
                 "temperature", 0.7
